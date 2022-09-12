371f91a557b4eab041ef90de5b005f1364feb945
Julio <juliornellas@gmail.com>
2022-09-12 09:07:31 -0300

    Update Read.me

---
+++ js/aula.js
@@ -0,0 +1,28 @@
+//Array
+const frutas = ["banana", "uva","manga", {nome: "Beatriz",
+apelido: ["Bia", "Bibi", "corinthiana"],
+telefone: ["01399101454" , "0133652487"],
+idade: 17,
+matriculada: true}];
+// console.log(frutas)
+/**
+ * Objeto
+ */
+let aluna = {
+    "nome": "Beatriz",
+    "apelido": ["Bia", "Bibi", "corinthiana"],
+    "telefone": [13991014541 , 1336524871],
+    "idade": 17,
+    "matriculada": true
+}
+
+let aluno = {
+    "nome": "David",
+    "apelido": "",
+    "telefone": 13991654151,
+    "idade": 17,
+    "matriculada": true
+};
+
+console.log(aluna.apelido[1])
+console.log(aluno['telefone'])
